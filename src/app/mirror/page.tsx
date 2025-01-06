9d6d9cb18748de1cddd133d6663596800180239d
chinoel <tnehd83@naver.com>
2025-01-06 11:05:10 +0900

    feat:구성 변경

--- src/app/mirror/page.tsx
+++ src/app/mirror/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+import { useEffect, useRef } from "react";
+
 export default function Admin() {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+    
+    useEffect(() => {
+        const initCamera = async() => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({video:true});
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        initCamera();
+    });
+
     return (
-      <>
-        <div>
-          test
-        </div>
-      </>
+        <>
+            <video ref={videoRef} autoPlay playsInline />
+        </>
     );
-  }
-  
+}
