57cfef8cc02f9b3c710acad339b4d2ac81a2e96b
chinoel <tnehd83@naver.com>
2025-01-06 10:52:30 +0900

    feat:에러 페이지 추가

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,9 @@
 import type { NextConfig } from "next";
 
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/:path',
-        destination: '/',
-        permanent: false,
-      },
-    ]
-  }
-}
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true,
 };
 
 export default nextConfig;
