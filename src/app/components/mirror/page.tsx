14083ad570a09e8570e8b7b70bc7c0ee32898462
chinoel <tnehd83@naver.com>
2025-03-24 17:34:59 +0900

    feat:send to back mirror data

--- src/app/components/mirror/page.tsx
+++ src/app/components/mirror/page.tsx
@@ -21,6 +21,25 @@ export default function Mirror() {
         }
     }
 
+    const dataSend = async (data: any) => {
+        try {
+            const response = await fetch('http://localhost:5000/homes/usercheck', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            // return 값 확인
+            const result = await response.json();
+            console.log(result);
+        }
+        catch (err) {
+            console.error(err)
+        }
+    }
+
     // Load the models (모델 로드)
     const loadModels = async () => {
         try {
@@ -67,7 +86,7 @@ export default function Mirror() {
                     }
 
                     if (resizedDetections.length > 0) {
-                        console.log(resizedDetections)
+                        dataSend(resizedDetections)
                         setNotification('얼굴이 감지되었습니다.')
                         setMirrorMode(1)
                     }
